8a04d980684d5ef08b6dd47b7bc715bc0b65d3cd
NKLam <khoalamvn@gmail.com>
2022-08-03 14:47:32 +1000

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,15 @@
+from scipy.signal import welch, freqz, butter, filtfilt
+
+# For filtering the data
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype="band")
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = filtfilt(b, a, data)
+    return y
